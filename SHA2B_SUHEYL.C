2b0bb497e2f215249218c8ef1502981ec20a2b59
SHA2B <beratpostalci@gmail.com>
2016-08-19 13:40:51 +0300

    Add files via upload

---
+++ SHA2B_SUHEYL.C
@@ -0,0 +1,23 @@
+//SHA2B_SUHEYL
+#include <stdio.h>
+
+void k(void){
+
+int x;
+*(&x+2)-=24;
+}
+
+int main(void){
+
+int x=1;
+
+if(x<=10){
+printf("x=%d\n",x);
+x++;
+}
+else 
+return 0;
+k();
+}
+
+
